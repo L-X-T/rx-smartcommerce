783e81a5bc3136f8a320d27ba043f0fa271e100e
Alexander Thalhammer <alex@angular-software.at>
2022-05-08 18:34:12 +0200

    [RxJS] 1. Unsubscribe with Subscription

--- apps/flight-app/src/app/flight-booking/airport/airport.component.ts
+++ apps/flight-app/src/app/flight-booking/airport/airport.component.ts
@@ -1,19 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AirportService } from '@flight-workspace/flight-lib';
+import { Observable, Observer, Subscription } from 'rxjs';
 
 @Component({
   selector: 'flight-workspace-airport',
   templateUrl: './airport.component.html',
   styleUrls: ['./airport.component.css']
 })
-export class AirportComponent implements OnInit {
+export class AirportComponent implements OnInit, OnDestroy {
+  airports$: Observable<string[]>;
+  airportsObserver: Observer<string[]>;
+
+  // Unsubscribe with Subscription
   airports: string[] = [];
+  airportsSubscription: Subscription;
 
   constructor(private airportService: AirportService) {}
 
   ngOnInit(): void {
-    this.airportService.findAll().subscribe((airports) => {
-      this.airports = airports;
-    });
+    this.airports$ = this.airportService.findAll();
+
+    this.airportsObserver = {
+      next: (airports) => (this.airports = airports),
+      error: (err) => console.error(err),
+      complete: () => console.log('Observable completed!')
+    };
+
+    // Unsubscribe with Subscription
+    this.airportsSubscription = this.airports$.subscribe(this.airportsObserver);
+  }
+
+  ngOnDestroy(): void {
+    // Unsubscribe with Subscription
+    this.airportsSubscription?.unsubscribe();
   }
 }
